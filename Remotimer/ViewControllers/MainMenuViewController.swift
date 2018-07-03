e4497bf522849e129ef6d0c6f50141459d8c48b2
iamchiwon <iamchiwon@itsdcode.com>
2018-07-03 18:57:35 +0900

    initial commit

---
+++ Remotimer/ViewControllers/MainMenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainMenuViewController.swift
+//  Remotimer
+//
+//  Created by iamchiwon on 2018. 7. 3..
+//  Copyright © 2018년 ncode. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func mainRewindSegue(segue: UIStoryboardSegue) {
+        //nothing
+    }
+}
---
+++ Remotimer/ViewControllers/TimerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimerViewController.swift
+//  Remotimer
+//
+//  Created by iamchiwon on 2018. 7. 3..
+//  Copyright © 2018년 ncode. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
